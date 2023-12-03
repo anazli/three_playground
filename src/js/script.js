9128c5580bb14f29305203ded78ff65d51d41307
Andreas <andreas221b@gmail.com>
2023-12-03 15:06:12 +0100

    Add gui staff, lights and shadows

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 const axesHelper = new THREE.AxesHelper(5);
@@ -18,26 +20,61 @@ const box = new THREE.Mesh(boxGeometry, boxMeterial);
 scene.add(box);
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide});
+const planeMaterial = new THREE.MeshStandardMaterial({color: 0xFFFFFF, side: THREE.DoubleSide});
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
 scene.add(plane);
 
 const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x0000FF, wireframe: false})
+const sphereMaterial = new THREE.MeshStandardMaterial({color: 0x0000FF, wireframe: false})
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.position.x = -10;
+sphere.position.set(-10, 10, 0);
+sphere.castShadow = true;
 scene.add(sphere);
 
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+directionalLight.position.set(-30, 50, 0);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.bottom = -12;
+scene.add(directionalLight);
+
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(dLightHelper);
+
 const gridHelper = new THREE.GridHelper(30);
-gridHelper.rotation.x = -0.5 * Math.PI;
+//gridHelper.rotation.x = -0.5 * Math.PI;
 scene.add(gridHelper);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
 
+const gui = new dat.GUI();
+const options = {
+    sphereColor: '#ffea00',
+    wireframe: false,
+    speed: 0.01
+};
+gui.addColor(options, 'sphereColor').onChange(function(e) {
+    sphere.material.color.set(e);
+})
+
+gui.add(options, 'wireframe').onChange(function(e) {
+    sphere.material.wireframe = e;
+})
+
+gui.add(options, 'speed', 0, 0.1)
+let step = 0;
+
 function animate() {
     box.rotation.x += 0.01;
     box.rotation.y += 0.01;
+
+    step += options.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(step));
     renderer.render(scene, camera);
 }
 
